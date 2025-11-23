597824152fccf95399ecd9de44d272519c630928
huseynln <huseynhastaken@gmail.com>
2025-11-23 13:13:24 +0000

    f

---
+++ python-classes/2-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Module that defines a Square class with size validation"""
+
+
+class Square:
+    """A class that defines a square by its size
+
+    Attributes:
+        __size: Private attribute representing the size of the square
+    """
+
+    def __init__(self, size=0):
+        """Initialize a new Square instance with validation
+
+        Args:
+            size: The size of the square (default: 0)
+
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculate and return the area of the square
+
+        Returns:
+            The area of the square (size * size)
+        """
+        return self.__size ** 2
